a096e776cbb07b57547e451fdad4e5f2811290da
CRISTIANO MELO <99894054+Cristiano-Melo@users.noreply.github.com>
2022-09-30 19:28:55 -0300

    Initial commit

---
+++ MenuJdesktop/src/Conexao/Dao/ClienteDao.java
@@ -0,0 +1,25 @@
+package Conexao.Dao;
+
+import Models.Cliente;
+
+public class ClienteDao {
+
+	private ConectaBancoDao conectabancodao = new ConectaBancoDao();
+
+	public void inserirCliente(Cliente cliente) {
+		try {
+
+			String query = "insert into clientes values(null, '" + cliente.getNome_cliente() + "','"
+					+ cliente.getCpf_cliente() + "','" + cliente.getRg_cliente() + "','" + cliente.getEmail_cliente()
+					+ "','" + cliente.getTelefone_cliente() + "','" + cliente.getEndereco_cliente() + "','"
+					+ cliente.getBairro_cliente() + "','" + cliente.getCidade_cliente() + "','"
+					+ cliente.getUf_cliente() + "','" + cliente.getCep_cliente() + "');";
+			System.out.println(query);
+			conectabancodao.getStatement().execute(query);
+
+		} catch (Exception e) {
+			System.out.println("ERRO: " + e.getMessage());
+		}
+
+	}
+}
