f06318f46186b8016e3094c00b633451e3769d1d
CRISTIANO MELO <99894054+Cristiano-Melo@users.noreply.github.com>
2022-10-04 17:27:17 -0300

    Teste

--- MenuJdesktop/src/Conexao/Dao/ClienteDao.java
+++ MenuJdesktop/src/Conexao/Dao/ClienteDao.java
@@ -22,4 +22,13 @@ public class ClienteDao {
 		}
 
 	}
+	
+	public void pesquisarTodosClientes() {
+		try {
+			String query = "select * from clientes;";
+			conectabancodao.getStatement().execute(query);
+		} catch (Exception e) {
+			System.out.println("ERRO: " + e.getMessage());
+		}
+	}
 }
